69d685e88517701e8cc23beee43ed08121573381
Moye <1154268595@qq.com>
2023-03-07 00:56:37 +0800

    add Unit test 001 IO

---
+++ Test/Unit Tests/Unit_Test_001 (IO)/test.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+
+class TestIO : public testing::Test
+{
+protected:
+	void SetUp() override
+	{
+	}
+
+	void TearDown() override
+	{
+	}
+};
+
+TEST(TestIO, DT_InvalidInput)
+{
+
+}
